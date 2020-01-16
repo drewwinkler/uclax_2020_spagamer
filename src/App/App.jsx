2e263534242f9a06487fd79a708550d870086482
Drew Winkler <drewwinkler4@gmail.com>
2020-01-15 21:32:03 -0800

    added media queries

---
+++ src/App/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.scss'
+
+const App = () => {
+
+    return (
+        <div className='wrapper'>
+        <header className='header'>HEADER FOR THE APP</header>
+        <nav className='nav'>NAV FOR THE APP</nav>
+        <main className='main'>MAIN FOR THE APP</main>
+        <footer className='footer'>FOOTER FOR THE APP</footer>
+        </div>
+    )
+}
+
+export default App;
